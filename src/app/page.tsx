d51d969156d3aa8f102948c678ed96b9a956c3d8
neosharma995 <mytravplan@gmail.com>
2024-10-08 16:46:32 +0530

    code is updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,18 @@
- 
-
 export default function Home() {
   return (
-  <>
-                                     <h1>dfasdfasdfasdf</h1>
-                                     <h1>dfasdfasdfasdf</h1>
+    <>
+      <h1>dfasdfasdfasdf</h1>
+      <h1>dfasdfasdfasdf</h1>
 
-
-  <h1>this is a prisma tutorial</h1>
-                                                                                    <h1>this is a prisma tutorial</h1>
-  <h1>this is a prisma tutorial</h1>
-                                                                                          <h1>this is a prisma tutorial</h1>
-  <h1>this is a prisma tutorial</h1>
-        <h1>this is a prisma tutorial</h1>
-  <h1>this is a prisma tutorial</h1>
-                                                                                                       <h1>this is a prisma tutorial</h1>
-  <h1>this is a prisma tutorial</h1>
-  </>
+      <h1>this is a prisma tutorial</h1>
+      <h1>this is a prisma tutorial</h1>
+      <h1>this is a prisma tutorial</h1>
+      <h1>this is a prisma tutorial</h1>
+      <h1>this is a prisma tutorial</h1>
+      <h1>this is a prisma tutorial</h1>
+      <h1>this is a prisma tutorial</h1>
+      <h1>this is a prisma tutorial</h1>
+      <h1>this is a prisma tutorial</h1>
+    </>
   );
 }
-                 
-
